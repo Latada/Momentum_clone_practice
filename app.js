1f927f403785a7a14c311c95684e6cedb00d8b04
Latada <icom92@naver.com>
2022-08-20 22:50:34 +0900

    로그인 기능

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
-const loginForm = document.querySelector("#login-form");
-const loginInput = document.querySelector("#login-form input");
+const loginForm = document.querySelector('#login-form');
+const loginInput = loginForm.querySelector('input');
+const Greeting = document.querySelector('#greeting');
 
-function onLoginSubmit(event) {
+const hiddenClassName = "hidden";
+const userNameKey = "이름";
+
+function onLoginSubmit (event) {
   event.preventDefault();
+  loginForm.classList.add(hiddenClassName);
   const username = loginInput.value;
-  console.log(username)
+  localStorage.setItem(userNameKey, username);
+  paintGreetings(username);
 }
 
-loginForm.addEventListener("click", onLoginSubmit);
+function paintGreetings(username) {
+  Greeting.innerText = `안녕 ${username}`; //string이랑 함께 넣을때 ${} 필수
+  Greeting.classList.remove(hiddenClassName);
+}
+
+const savedUserName = localStorage.getItem(userNameKey);
+
+if (savedUserName === null) {
+  //입력창 보이기
+  loginForm.classList.remove(hiddenClassName);
+  loginForm.addEventListener("submit", onLoginSubmit);
+} else {
+  //인삿말 보이기
+  paintGreetings(savedUserName);
+}
